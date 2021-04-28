be2ba4defebeaacdc25c0e0f3bed67e2e8eebd89
Gabriel Kolber <gabrielkolber@icloud.com>
2021-04-28 19:53:31 -0300

    feat: created a new view render to post method and now show.json returns the value with subtree

--- challenge/lib/challenge_web/views/entitie_view.ex
+++ challenge/lib/challenge_web/views/entitie_view.ex
@@ -6,8 +6,18 @@ defmodule ChallengeWeb.EntitieView do
     %{data: render_many(entities, EntitieView, "entitie.json")}
   end
 
-  def render("show.json", %{entitie: entitie}) do
+  def render("post.json", %{entitie: entitie}) do
+    %{id: entitie.id,}
     %{data: render_one(entitie, EntitieView, "entitie.json")}
+  end
+
+  def render("show.json", %{entitie: entitie, subtree: subtree}) do
+    %{id: entitie.id,
+      name: entitie.name,
+      entity_type: entitie.entity_type,
+      inep: entitie.inep,
+      parent_id: entitie.parent_id,
+      subtree: subtree}
   end
 
   def render("entitie.json", %{entitie: entitie}) do
